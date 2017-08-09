51e907d284456b91d11f0326726a49e538b9e53a
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-09 16:21:40 +0200

    fixup! Add npm package for publishing

--- publish.js
+++ publish.js
@@ -8,6 +8,8 @@ ghpages.publish('public',{
 	branch: 'gh-pages',
 	message: 'Auto-commit from master branch'
 	},function(err) {
-    	console.error(err)
-    	process.exit(1)
+		if(err){
+			console.error(err)
+    		process.exit(1)
+		}
 	});
