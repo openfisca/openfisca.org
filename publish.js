8e04f4b21223c7946d46f29b6f4518741b2fcb93
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-09 16:04:56 +0200

    Make package follow conventions

--- publish.js
+++ publish.js
@@ -1,10 +1,13 @@
 var ghpages = require('gh-pages');
 var path = require('path');
- 
-ghpages.publish('to_be_published', {
+
+ghpages.publish('public',{
 	user: {
-    name: 'OpenFisca-Bot',
-  	},
-  	branch: 'gh-pages',
-  	message: 'Auto-commit from master branch'
-}, function(err) {});
+		name: 'OpenFisca-Bot',
+	},
+	branch: 'gh-pages',
+	message: 'Auto-commit from master branch'
+	},function(err) {
+    	console.error(err)
+    	process.exit(1)
+	});
