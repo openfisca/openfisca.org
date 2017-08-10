c9d3764e2870920a55616929c232837503071a1c
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-10 19:12:14 +0200

    Enhance readability

--- publish.js
+++ publish.js
@@ -1,15 +1,14 @@
 var ghpages = require('gh-pages');
-var path = require('path');
 
-ghpages.publish('public',{
-	user: {
-		name: 'OpenFisca-Bot',
-	},
-	branch: 'gh-pages',
-	message: 'Auto-commit from master branch'
-	},function(err) {
-		if(err){
-			console.error(err)
-    		process.exit(1)
-		}
-	});
+ghpages.publish('public', {
+  user: {
+    name: 'OpenFisca-Bot',
+  },
+  branch: 'gh-pages',
+  message: 'Auto-commit from master branch'
+}, function(err) {
+  if(err){
+  console.error(err)
+  process.exit(1)
+  }
+})
