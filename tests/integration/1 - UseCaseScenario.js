670a19beb15ea84c7643f6f94210f68e9bc37523
sandcha <sandcha@users.noreply.github.com>
2017-08-18 19:35:18 +0200

    Add integration tests
    And use the shiny image for revenu-de-base

---
+++ tests/integration/1 - UseCaseScenario.js
@@ -0,0 +1,9 @@
+description: "A use case section must have a title, an illustration legend and a link",
+
+steps: [
+  {
+    "UseCaseComponent.caseTitle": citizensCaseTitle,
+    "UseCaseComponent.caseDescription": citizensCaseDescription,
+    "UseCaseComponent.caseFigureLink": citizensCaseFigureLink,
+  }
+]
---
+++ tests/integration/UseCaseComponent.js
@@ -0,0 +1,3 @@
+caseTitle: "section div h3",
+caseDescription: "section div p",
+caseFigureLink: "section figure figcaption a"
