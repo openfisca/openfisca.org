24a43ca749370c494d94057b20478aae128d1015
sandcha <sandcha@users.noreply.github.com>
2017-08-29 16:52:50 +0200

    Correct tests on countries page

--- tests/integration/CountriesComponent.js
+++ tests/integration/CountriesComponent.js
@@ -1,9 +1,9 @@
-frInfo: "a[href=https://github.com/openfisca/openfisca-france]",
-frLegislationExplorer: "a[href=https://legislation.openfisca.fr]",
+frInfo: "a[href='https://github.com/openfisca/openfisca-france']",
+frLegislationExplorer: "a[href='https://legislation.openfisca.fr']",
 
-tnInfo: "a[href=href=https://github.com/openfisca/openfisca-tunisia]",
-tnLegislationExplorer: "a[href=https://legislation.openfisca.tn]",
+tnInfo: "a[href='https://github.com/openfisca/openfisca-tunisia']",
+tnLegislationExplorer: "a[href='https://legislation.openfisca.tn']",
 
-snInfo: "div p a[href=https://github.com/openfisca/senegal]",
+snInfo: "div p a[href='https://github.com/openfisca/senegal']",
 
 goHomeLink: {a: "Home"},
