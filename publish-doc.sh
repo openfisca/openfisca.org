34c111290be08003333562899286e4f04ca37efe
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-30 21:11:30 +0200

    Clarify options in publish-doc

--- publish-doc.sh
+++ publish-doc.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-git clone -b gh-pages https://github.com/openfisca/openfisca.org.git
+git clone --branch gh-pages https://github.com/openfisca/openfisca.org.git
 # this next line clones the "doc-html" branch.
 # "doc-html" contains the latest build of the doc's html.
-git clone -b doc-html https://github.com/openfisca/openfisca.org.git doc-file
-rm -rf openfisca.org/doc
+git clone --branch doc-html https://github.com/openfisca/openfisca.org.git doc-file
+rm --recursive --force openfisca.org/doc
 mv doc-file/doc openfisca.org/doc
 cd openfisca.org
 git add .
 git config --global user.name "OpenFisca-Bot"
 git config --global user.email "contact@openfisca.fr"
-git commit -m "Add latest doc version"
+git commit --message="Add latest doc version"
 git push https://github.com/openfisca/openfisca.org.git gh-pages
