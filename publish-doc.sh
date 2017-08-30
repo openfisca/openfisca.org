dbba79090341fc7ace904eb6b862addaeb19fa4f
Anna-Livia <annalivia.cardin@gmail.com>
2017-08-30 20:23:09 +0200

    Explain the process of updating the doc

--- publish-doc.sh
+++ publish-doc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 git clone -b gh-pages https://github.com/openfisca/openfisca.org.git
+# this next line clones the "doc-html" branch.
+# "doc-html" contains the latest build of the doc's html.
 git clone -b doc-html https://github.com/openfisca/openfisca.org.git doc-file
 rm -rf openfisca.org/doc
 mv doc-file/doc openfisca.org/doc
@@ -10,4 +12,3 @@ git config --global user.name "OpenFisca-Bot"
 git config --global user.email "contact@openfisca.fr"
 git commit -m "Add latest doc version"
 git push https://github.com/openfisca/openfisca.org.git gh-pages
-
