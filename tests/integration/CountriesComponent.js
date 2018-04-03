064f8700ec37a88dad4a25c2777a665ddb5c237c
Anna-Livia <annalivia.gomart@gmail.com>
2018-04-03 15:31:45 +0200

    Update legislation explorer link

--- tests/integration/CountriesComponent.js
+++ tests/integration/CountriesComponent.js
@@ -1,5 +1,5 @@
 frInfo: "a[href='https://github.com/openfisca/openfisca-france']",
-frLegislationExplorer: "a[href='https://legislation.openfisca.fr']",
+frLegislationExplorer: "a[href='https://fr.openfisca.org/legislation']",
 
 tnInfo: "a[href='https://github.com/openfisca/openfisca-tunisia']",
 tnLegislationExplorer: "a[href='https://legislation.openfisca.tn']",
