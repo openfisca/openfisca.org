821b1a3e5efafd5a10f23a7d65b865c6309af2c3
sandcha <sandcha@users.noreply.github.com>
2019-03-29 11:35:18 +0100

    Update tests configuration on CI due to Android upgrade

--- tests/integration/config-ci.js
+++ tests/integration/config-ci.js
@@ -1,26 +1,27 @@
 // this file is for use in CircleCI continuous integration environment
 
-var capabilities = [
-  {browserName: 'chrome'},
-  {browserName: 'firefox'},
-  {browserName: 'internet explorer'},
-  {
-    deviceName: 'Samsung Galaxy S6 GoogleAPI Emulator',
-    browserName: 'Chrome',
-    platformName: 'Android',
-    platformVersion: '7.0'
-  }
-  ][process.env.CIRCLE_NODE_INDEX];
+var browserName = ['chrome', 'firefox', 'internet explorer', 'android'][process.env.CIRCLE_NODE_INDEX];
+
+var tunnel_id = 'circle-' + process.env.CIRCLE_BUILD_NUM + '-' + process.env.CIRCLE_NODE_INDEX;
+var capabilities = browserName == 'android' ?
+	  {
+	    'tunnel-identifier': tunnel_id,
+	    browserName: browserName,
+	    version: '6.0'
+	  }
+	  :
+	  {
+	    'tunnel-identifier': tunnel_id,
+	    browserName: browserName
+	  };
+
 
 module.exports = {
   seleniumServerURL: {
     hostname: 'ondemand.saucelabs.com',
     port: 80
   },
-  driverCapabilities: Object.assign({},
-    capabilities,
-    {'tunnel-identifier': 'circle-' + process.env.CIRCLE_BUILD_NUM + '-' + process.env.CIRCLE_NODE_INDEX}
-  ),
+  driverCapabilities: capabilities,
   tags: [ 'circle-ci', '#' + process.env.CIRCLE_BUILD_NUM ],
   views: [ 'Verbose', 'SauceLabs' ],
   quit: 'always', // avoid wasting 90 seconds on SauceLabs
