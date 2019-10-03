c2872753292f5d2f57a6011138e22e025835af25
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-10-03 15:44:26 +0200

    Fix deployment

--- tests/integration/1 - UseCaseScenario.js
+++
@@ -1,9 +0,0 @@
-description: "A use case section must have a title, a description and an illustration reference.",
-
-steps: [
-  {
-    "UseCaseComponent.caseTitle": citizensCaseTitle,
-    "UseCaseComponent.caseDescription": citizensCaseDescription,
-    "UseCaseComponent.caseFigureLink": citizensCaseFigureLink,
-  }
-]
--- tests/integration/CountriesComponent.js
+++
@@ -1,9 +0,0 @@
-frInfo: "a[href='https://github.com/openfisca/openfisca-france']",
-frLegislationExplorer: "a[href='https://fr.openfisca.org/legislation']",
-
-tnInfo: "a[href='https://github.com/openfisca/openfisca-tunisia']",
-tnLegislationExplorer: "a[href='https://legislation.openfisca.tn']",
-
-snInfo: "a[href='https://github.com/openfisca/senegal']",
-
-goHomeLink: "a[id='home']",
