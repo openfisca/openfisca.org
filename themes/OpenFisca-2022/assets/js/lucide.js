da27a38ba61d0ee5f1d5546c15e9a35c029c94cf
Clément Biron <clement.biron@gmail.com>
2022-12-15 14:35:23 +0100

    Add icons

---
+++ themes/OpenFisca-2022/assets/js/lucide.js
@@ -0,0 +1,26 @@
+import {
+		Github,
+		Landmark,
+		LineChart,
+		Menu,
+		Package,
+		Slack,
+		TerminalSquare,
+		Twitter,
+		X,
+		createIcons
+} from 'lucide';
+
+createIcons({
+	icons : {
+		Twitter,
+		Github,
+		Slack,
+		Menu,
+		X,
+		Landmark,
+		TerminalSquare,
+		LineChart,
+		Package
+	}
+});
