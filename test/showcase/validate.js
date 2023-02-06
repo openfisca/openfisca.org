ac94952a52d9266c69bef04c89f89504f00644ad
Matti Schneider <matti.schneider@beta.gouv.fr>
2023-02-06 16:55:59 +0100

    Lint JavaScript

--- test/showcase/validate.js
+++ test/showcase/validate.js
@@ -1,5 +1,5 @@
 import {readdirSync, statSync} from 'node:fs';
-import {basename} from 'node:path'
+import {basename} from 'node:path';
 
 import expect from 'expect.js';
 import imageSize from 'image-size';
@@ -14,7 +14,7 @@ const ILLUSTRATIONS_DIR = 'static/img/showcase';
 describe('Showcase entries', () => {
   const entries = readdirSync(SHOWCASE_DIR);
 
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     entry = basename(entry, '.yml');
 
     describe(entry, () => {
@@ -34,14 +34,14 @@ describe('Showcase entries', () => {
         });
 
         it('is not too heavy', () => {
-          expect(stats.size).to.be.below(400 * 1024);  // 400 kiB
-        })
+          expect(stats.size).to.be.below(400 * 1024); // 400 kiB
+        });
 
         it('is large enough', () => {
           const dimensions = imageSize(path);
           expect(dimensions.width).to.be.greaterThan(1279);
           expect(dimensions.height).to.be.greaterThan(719);
-          expect(dimensions.width / dimensions.height).to.be.within(1.7, 1.8);  // expect a 16:9 ratio
+          expect(dimensions.width / dimensions.height).to.be.within(1.7, 1.8); // expect a 16:9 ratio
         });
       });
     });
