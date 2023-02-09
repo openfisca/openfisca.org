5a0cb6e80397d42b89d7ccf892824c0283a40a31
Clément Biron <clement.biron@gmail.com>
2023-02-09 18:24:30 +0100

    Test package file name

--- test/packages/validate.js
+++ test/packages/validate.js
@@ -23,6 +23,19 @@ describe('packages entries', () => {
         });
         expect(validationErrors).to.be.empty();
       });
+
+      it('is well named', () => {
+        // ^             start of the string
+        // [A-Z]{2}      two uppercase characters (ISO 3166-2)
+        // (-            optional: dash
+        // [A-Z0-9]{1,3} 1 to 3 alphanumeric characters (ISO 3166-2 subdivision)
+        // )?            end of optional
+        // (_            optional: underscore
+        // [A-Za-z0-9]+  at least 1 alphanumeric string
+        // )?            end of optional
+        // $             end of the string
+        expect(entry).to.match(/^[A-Z]{2}(-[A-Z0-9]{1,3})?(_[A-Za-z0-9]+)?$/);
+      });
     });
   });
 });
