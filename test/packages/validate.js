83dd369dbe3df8f16ed3d7e5f24ad81991d689db
Clément Biron <clement.biron@gmail.com>
2023-03-01 10:46:21 +0100

    Improve naming

--- test/packages/validate.js
+++ test/packages/validate.js
@@ -2,7 +2,7 @@ import {readdirSync} from 'node:fs';
 import {basename} from 'node:path';
 import expect from 'expect.js';
 import validateSchema from 'yaml-schema-validator';
-import pacakgesEntrySchema from './schema.js';
+import packageSchema from './schema.js';
 
 const PACKAGES_DIR = 'data/packages';
 
@@ -19,7 +19,7 @@ describe('packages entries', () => {
     describe(entry, () => {
       it('has a valid description', () => {
         const validationErrors = validateSchema(`${PACKAGES_DIR}/${entry}.yml`, {
-          schema: pacakgesEntrySchema,
+          schema: packageSchema,
         });
         expect(validationErrors).to.be.empty();
       });
