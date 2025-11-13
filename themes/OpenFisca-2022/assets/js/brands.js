36ca9d3b8498834d942f128b3519e0cfd306de21
Clément Biron <clement.biron@gmail.com>
2025-11-13 14:43:31 +0100

    Load brands icons

---
+++ themes/OpenFisca-2022/assets/js/brands.js
@@ -0,0 +1,30 @@
+import { siGithub, siMastodon, siSlack } from 'simple-icons';
+
+const ICONS = {
+  mastodon: siMastodon,
+  slack: siSlack,
+  github: siGithub,
+};
+
+async function applyBrandsIcons() {
+  const elements = document.querySelectorAll('[data-simple-icon]');
+
+  elements.forEach((element) => {
+    const attribute = element.getAttribute('data-simple-icon').toLowerCase();
+    const icon = ICONS[attribute];
+
+    if (icon) {
+      const svgDoc = new DOMParser().parseFromString(icon.svg, 'image/svg+xml');
+      const svgElement = svgDoc.querySelector('svg');
+      const existingClasses = element.getAttribute('class');
+
+      if (existingClasses) {
+        svgElement.setAttribute('class', existingClasses);
+      }
+      svgElement.setAttribute('fill', 'var(--svgfill)');
+      element.replaceWith(svgElement);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', applyBrandsIcons);
